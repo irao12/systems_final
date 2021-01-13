d88c0b9b51dc10fda8b4c694f0eca66a968deb88
irao12 <71599573+irao12@users.noreply.github.com>
2021-01-13 10:37:19 -0500

    Update functions.c

--- functions.c
+++ functions.c
@@ -25,6 +25,7 @@ int pass_length(char* buffer){
     i++;
     count++;
   }
+  printf("count: %d", count);
   return count;
 }
 
@@ -32,6 +33,7 @@ char * pass_fix(char * pass, int len){
   int i;
   char * new_pass = malloc(sizeof(char)*(len+1));
   for (i = 0; i<len;i++){
+    if (pass[i]>47 || pass[i]<123)
     new_pass[i] = pass[i];
   }
   new_pass[len+1] = 0;
@@ -39,12 +41,12 @@ char * pass_fix(char * pass, int len){
 }
 
 void create_acc(){
-  char usr[100];
+  char usr[50];
   char pass[31];
   int i;
-  for (i = 0; i<31;i++){
+ /* for (i = 0; i<31;i++){
     pass[i] = 0;
-  }
+  }*/
   int con = 1;
   while (con){
     printf("Enter a username for this account\n");
@@ -70,7 +72,36 @@ void create_acc(){
   }
 }
 
-/*void login(){
-
+void login(int * entry){
+  char usr[50], usr_entered[50];
+  char pass[31], pass_entered[31];
+  char location[100];
+  int con = 1;
+  FILE* fp;
+  while (con){
+    printf("\nEnter your username\n");
+    fgets(usr_entered, sizeof(usr_entered), stdin);
+    remover(usr_entered);
+    strcpy(location, "accounts/");
+    strcat(location, usr_entered);
+    fp = fopen(location, "r");
+    if (!fp){
+      printf("Account with that name does not exist\n");
+    }
+    else{
+      printf("\nEnter your password\n");
+      fgets(pass_entered, sizeof(pass_entered), stdin);
+      remover(pass_entered);
+      fgets(pass, sizeof(pass), fp);
+      remover(pass);
+      if (!strcmp(pass, pass_entered)){
+        printf("\nLogin successful!\n");
+        con = 0;
+        *entry = 1;
+      }
+      else{
+        printf("\nIncorrect password\n");
+      }
+    }
+  }
 }
-*/
