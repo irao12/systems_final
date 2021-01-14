3385ece16e07013a0c5d1bdaa89bc32455fb7469
irao12 <71599573+irao12@users.noreply.github.com>
2021-01-14 18:41:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #include <signal.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+
+int entry = 0;
 
 int main(void) {
 umask(0000);
@@ -15,12 +18,12 @@ while (1){
   char line[100];
   printf("Welcome to the Password Manager\n");
   int con = 1;
-  int entry = 0;
   char *user;
   while (con){
     printf("Enter \"login\" to log in or \"create\" to create a new account\nfor the password manager\n");
     fgets(line, sizeof(line), stdin);
     remover(line);
+
     if (!strcmp(line,"create")){
       create_acc();
       con = 0;
@@ -29,16 +32,29 @@ while (1){
       user = login(&entry);
       con = 0;
     }
-    
     else {
       printf("Try again\n");
     }
+
   }
-  while (entry){
-    char prompt[10];
-    printf("Would you like to \"see\", \"add\", or \"update\"\n");
-    fgets(prompt, sizeof(prompt), stdin);
+
+  int status;
+  int child;
+  child = fork();
+
+  if (!child){
+    while (entry){
+      char prompt[10];
+      printf("\nWould you like to \"see\", \"add\", or \"update\"\n");
+      fgets(prompt, sizeof(prompt), stdin);
+    }
+    return 0;
   }
+
+  else {
+    int childpid = wait(&status);
+  }
+
 }
   return 0;
 }
