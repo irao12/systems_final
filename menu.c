9e7ca9fcf9632a8637431481d354bbe47b2f5977
Alex Zhang <71724195+Kappachino-Latte@users.noreply.github.com>
2021-01-18 17:34:11 -0500

    Update menu.c

--- menu.c
+++ menu.c
@@ -8,6 +8,23 @@
 #include <errno.h>
 #include <dirent.h>
 
+char location[100];
+
+static void sigcreate (int signo) {
+	if (signo == SIGINT) {
+    printf ("\n\nExiting password manager...\n");
+    remove(location);
+    exit(0);
+	}
+}
+
+static void siglogin (int signo) {
+	if (signo == SIGINT) {
+    printf ("\n\nExiting password manager...\n");
+    exit(0);
+	}
+}
+
 void setup() {
   DIR* dir = opendir("data");
   if (!dir){
@@ -43,81 +60,129 @@ void remover(char * buffer) {
   trim (buffer);
 }
 
-void create_acc(){
+void create_acc() {
+  signal (SIGINT, sigcreate);
+
+  printf ("\nCreating account...\nType \"cancel\" at anytime to go back to main menu\n");
   char usr[50];
   char pass[31], re_pass[31];
   int i;
   int con = 1;
-  while (con){
-    printf("\nEnter a username for this account\n");
-    fgets(usr, sizeof(usr), stdin);
-    remover(usr);
-    char location[100];
-    strcpy(location, "data/accounts/");
-    strcat(location, usr);
-    int file = open(location, O_RDWR);
-    if (file>0) {
+  while (con) {
+    printf ("\nEnter a username for this account\n");
+    fgets (usr, sizeof(usr), stdin);
+    remover (usr);
+
+    if (!strcmp(usr, "cancel")) {
+      break;
+    }
+
+    strcpy (location, "data/accounts/");
+    strcat (location, usr);
+    int file = open (location, O_RDWR);
+    if (file > 0) {
       printf("An account already exists with this name\n");
     }
     else {
-      file = open(location, O_RDWR | O_CREAT, 0666);
-      while(1){
-        printf("\nEnter a password for this account (max 30 characters)\n");
-        fgets(pass,sizeof(pass), stdin);
-        printf("\nReenter to Confirm your password\n");
-        fgets(re_pass,sizeof(pass), stdin);
-        if (!strcmp(pass, re_pass)){
+      file = open (location, O_RDWR | O_CREAT, 0666);
+      while (1) {
+        printf ("\nEnter a password for this account (max 30 characters)\n");
+        fgets (pass, sizeof(pass), stdin);
+        remover(pass);
+
+        if (!strcmp(pass, "cancel")) {
           break;
         }
-        else printf("\nPasswords do not match");
+
+        printf ("\nRe-enter to confirm your password\n");
+        fgets (re_pass,sizeof(pass), stdin);
+        remover (re_pass);
+        
+        if (!strcmp(re_pass, "cancel")) {
+          break;
+        }
+
+        if (!strcmp(pass, re_pass)) {
+          break;
+        }
+        else printf ("\nPasswords do not match");
       }
-      write(file, pass, sizeof(pass));
-      char dat_loc[100];
-      strcpy(dat_loc, "data/");
-      strcat(dat_loc, usr);
-      int test = mkdir(dat_loc, 0777);
-      if (test == -1){
+
+      if (!strcmp(pass, "cancel") || !strcmp(re_pass, "cancel")) {
+        remove(location);
+        break;
+      }
+
+      write (file, pass, sizeof(pass));
+      char dat_loc [100];
+      strcpy (dat_loc, "data/");
+      strcat (dat_loc, usr);
+      int test = mkdir (dat_loc, 0777);
+      if (test == -1) {
         printf("error: %s", strerror(errno));
       }
       
-      printf("\nAccount has been created\n\n");
+      printf("\nAccount has been created!\n");
 
       con = 0;
     }
   }
 }
 
-char * login(int * entry){
+char * login(int * entry) {
+  signal (SIGINT, siglogin);
+
+  printf ("\nLogging in...\nType \"cancel\" at anytime to go back to main menu\n");
   char * usr_entered = malloc(sizeof(char)*51);
   char pass[31], pass_entered[31];
-  char location[100];
+  char location2[100];
   int con = 1;
   FILE* fp;
-  while (con){
+  while (con) {
     printf("\nEnter your username\n");
     fgets(usr_entered, sizeof(usr_entered), stdin);
     remover(usr_entered);
-    strcpy(location, "data/accounts/");
-    strcat(location, usr_entered);
-    fp = fopen(location, "r");
-    if (!fp){
+
+    if (!strcmp(usr_entered, "cancel")) {
+      con = 0;
+      break;
+    }
+
+    strcpy(location2, "data/accounts/");
+    strcat(location2, usr_entered);
+    fp = fopen(location2, "r");
+
+    if (!fp) {
       printf("Account with that name does not exist\n");
     }
-    else{
+    else {
       printf("\nEnter your password\n");
       fgets(pass_entered, sizeof(pass_entered), stdin);
       remover(pass_entered);
+      
+      if (!strcmp(pass_entered, "cancel")) {
+        con = 0;
+        break;
+      }
+
       fgets(pass, sizeof(pass), fp);
       remover(pass);
-      if (!strcmp(pass, pass_entered)){
+      if (!strcmp(pass, pass_entered)) {
         printf("\nLogin successful!\n");
         con = 0;
         *entry = 1;
       }
-      else{
+      else {
         printf("\nIncorrect password\n");
       }
     }
   }
+
+  if (!strcmp(usr_entered, "cancel") || !strcmp(pass_entered, "cancel")) {
+    char * backed_out = "canceled";
+    *entry = 0;
+    return backed_out;
+  }
+  
   return usr_entered;
 }
